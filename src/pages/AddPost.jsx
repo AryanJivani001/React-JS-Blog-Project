0c547abd47cfc5075c97aa020ddf646845aa74ec
AryanJivani001 <128927413+AryanJivani001@users.noreply.github.com>
2024-02-05 21:11:50 +0530

    Update AddPost.jsx

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -5,6 +5,7 @@ function AddPost() {
   return (
     <div className='py-8'>
         <Container>
+                            <h1 className="text-2xl font-bold text-center">ADD POST</h1>
             <PostForm />
         </Container>
     </div>
