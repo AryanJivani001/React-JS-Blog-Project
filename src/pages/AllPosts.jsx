39e93972d570ac2edc83c95c65b8192c6982e514
AryanJivani001 <128927413+AryanJivani001@users.noreply.github.com>
2024-02-05 21:10:09 +0530

    Update AllPosts.jsx

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -13,6 +13,7 @@ function AllPosts() {
   return (
     <div className='w-full py-8'>
         <Container>
+                            <h1 className="text-2xl font-bold text-center">ALL POSTS</h1>
             <div className='flex flex-wrap'>
                 {posts.map((post) => (
                     <div key={post.$id} className='p-2 w-1/4'>
